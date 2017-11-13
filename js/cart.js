01a1972c6a7677a4cf5c3fd0121ebb5a358906d6
Anastasiia-derymarko <lolololo2263@gmail.com>
2017-11-13 09:37:25 +0200

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -5,39 +5,71 @@ function loadCart() {
   if (localStorage.getItem('cart')) {
     //если есть - расшифровываю и записываю в переменую cart
     cart = JSON.parse(localStorage.getItem('cart'));
-    showCart();
-  }
+    //прововерка корзины на постоту
+       	showCart();
+    }
   // если корзина пуста сказать 
   else{
     $('table').html('корзина пуста')
   }
 }
+
 function showCart(){
-  $.getJSON("js/product.json", function (data){
-    
-    var product = data;
-
-    var out = '';
-    for ( var id in cart){
-      out+=`          
-              <tr>
-                <td>
-                 <img src="${product[id].img}">
-                 <span>${product[id].name}</span>
-                </td>
-                <td>
-                  <a class="z-depth-1 cyan white-text bot bold" href="#">-</a>
-                  <span class="bold mar">1</span>
-                  <a class="z-depth-1 cyan white-text bot bold" href="#">+</a>
-                </td>
-                <td>${product[id].cost}грн.</td>
-              </tr>
-      `
-
+	if(!isEnpty(cart)){
+    	$('table').html('корзина пуста')
     }
-    $('.tbody').html(out);
-  });
+    else{
+		  	$.getJSON("js/product.json", function (data){
+		    
+		    var product = data;
+		    var out = '';
+			    for ( var id in cart){
+			      out+=`          
+			             <tr>
+			                <td><button class="z-depth-1 cyan white-text bold del-product" data-id="${id}" >x</button></td>
+			                <td>
+			                
+			                 <img src="${product[id].img}">
+			                 <span>${product[id].name}</span>
+			                </td>
+			                <td>
+			                  <button class="z-depth-1 cyan white-text bot bold" href="#">-</button>
+			                  <span class="bold mar">${cart[id]}</span>
+			                  <button class="z-depth-1 cyan white-text bot bold" href="#">+</button>
+			                </td>
+			                <td>${product[id].cost}грн.</td>
+			              
+			             </tr>
+			      `
+			    }
+			$('.tbody').html(out);
+			$('.del-product').on('click', delProduct);
+			});
+	}
+	
+};
+
+
+function delProduct(){
+  // удаляем товар из корзины
+  var id = $(this).attr('data-id');
+  delete cart[id];
+  saveCart();
+  showCart();
+
 }
+
+function saveCart() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+
+}
+function isEnpty(object){
+	//прововерка корзины на постоту
+	for(var key in object)
+	if(object.hasOwnProperty(key)) return true;
+	return false;	 
+}
+
 $(document).ready(function(){
   loadCart();
 });
